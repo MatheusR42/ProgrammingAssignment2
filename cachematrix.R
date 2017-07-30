0e3449190dca69603fad8fa79faf39ef2e1b1b3c
MatheusR42 <matheus.araujo.r42@gmail.com>
2017-07-30 11:05:09 -0300

    Assignment: Caching the Inverse of a Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# Create a "special matrix", wich is really a list
+# the "special matrix" has the the following functions:
+# set, get, setInverse and getInverse
+# it also caches the inverse matrix
 makeCacheMatrix <- function(x = matrix()) {
+	inverseMatrix <- NULL
 
+    set <- function(y) {
+        x <<- y
+        inverseMatrix <<- NULL
+    }
+    
+    get <- function(){
+    	x
+    } 
+    
+    setInverse <- function(inverse){
+    	inverseMatrix <<- inverse
+    }
+
+    getInverse <- function(){
+    	inverseMatrix
+    } 
+
+    list(set = set, 
+    	 get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
+# @param x is a "special matrix" made by makeCacheMatrix function
+# Return a matrix that is the inverse of the parameter
+cacheSolve <- function(x) {
+	inverse <- x$getInverse()
 
-## Write a short comment describing this function
+    if(!is.null(inverse)) {
+        message("getting cached data")
+        return(inverse)
+    }
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    data <- x$get()
+	inverse <- solve(data)
+	
+	x$setInverse(inverse)
+	inverse
 }
+
+tediousMatrix <- matrix(c(3, -7, 5, 2), 2, 2)
+specialMatrix <- makeCacheMatrix()
+specialMatrix$set(tediousMatrix)
+cacheSolve(specialMatrix)
